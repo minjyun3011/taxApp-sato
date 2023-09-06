8a5e6a04d983ecec4c373ce1af86cfe4799d190a
minjyun3011 <grand3canyon10@gmail.com>
2023-09-06 13:58:45 +0900

    Initial Commit

--- taxApp-sato/ViewController.swift
+++ taxApp-sato/ViewController.swift
@@ -7,13 +7,63 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var taxSwitchButton: UISegmentedControl!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var prices: [Double] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "priceCell")
     }
 
-
+    @IBAction func taxSwitchButton(_ sender: UISegmentedControl) {
+        calculateTax()
+    }
+    @IBAction func addButton(_ sender: UIButton) {
+        if let priceText = resultLabel.text, let price = Double(priceText) {
+            prices.append(price)
+            tableView.reloadData()
+        }
+    }
+    
+    @IBAction func totalButton(_ sender: UIBarButtonItem) {
+        performSegue(withIdentifier: "showLabel", sender: nil)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showTotal" {
+            let destinationVC = segue.destination as! TotalTableViewController
+            destinationVC.totalAmount = prices.reduce(0, +)
+        }
+    }
+    
+    func calculateTax() {
+        guard let priceText = textField.text, let price = Double(priceText) else {
+            return
+        }
+        let taxRate = taxSwitchButton.selectedSegmentIndex == 0 ? 0.10 : 0.08
+        let taxAmount = price * taxRate
+        let totalPrice = price + taxAmount
+        resultLabel.text = String(totalPrice)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return prices.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "priceCell", for: indexPath)
+        cell.textLabel?.text = String(prices[indexPath.row])
+        return cell
+    }
 }
 
